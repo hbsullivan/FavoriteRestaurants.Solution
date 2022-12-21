c01e5cba9484afa12904fb1812609cd9c3cf3925
Brian Noh <noh24@ymail.com>
2022-12-21 14:56:02 -0500

    add properties & reference navigation property
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

---
+++ FavoriteRestaurants/Models/Restaurant.cs
@@ -0,0 +1,12 @@
+namespace FavoriteRestaurants.Models
+{
+  public class Restaurant
+  {
+    public string Name { get; set; }
+    public string PriceRange { get; set; }
+    public string Rating { get; set; }
+    public int CuisineId { get; set; }
+    public int RestaurantId { get; set; }
+    public Cuisine Cuisine { get; set; }
+  }
+}
