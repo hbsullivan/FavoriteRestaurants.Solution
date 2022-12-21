4046d27cdeb74d4c0021e842b70edba944faf34b
Brian Noh <noh24@ymail.com>
2022-12-21 17:43:44 -0500

    change Rating to int
    Co-authored-by: Anton Ch <anton3ch@icloud.com>
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- FavoriteRestaurants/Models/Restaurant.cs
+++ FavoriteRestaurants/Models/Restaurant.cs
@@ -4,7 +4,7 @@ namespace FavoriteRestaurants.Models
   {
     public string Name { get; set; }
     public string PriceRange { get; set; }
-    public string Rating { get; set; }
+    public int Rating { get; set; }
     public int CuisineId { get; set; }
     public int RestaurantId { get; set; }
     public Cuisine Cuisine { get; set; }
